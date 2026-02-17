07c8d6b7f3e5e6ca90abba5161683f997755fb83
Mammoth70 <andrey-yakovlev@yandex.ru>
2026-02-17 08:25:45 +0300

    Version 2.6.3

--- app/src/main/java/ru/mammoth70/totpgenerator/Util.kt
+++ app/src/main/java/ru/mammoth70/totpgenerator/Util.kt
@@ -19,7 +19,7 @@ fun isValidBase32(secret: String): Boolean {
     // Проверка кратности согласно RFC 4648.
     // Полный блок Base32 должен быть кратен 8 символам.
     // Если padding отсутствует (что часто в QR), длина должна быть 2, 4, 5, 7 символов в остатке от деления на 8.
-    // Длины с остатком 1, 3, 6 — некорректны.
+    // Длины с остатком 1, 3, 6 - некорректны.
     if (secret.contains("=") && secret.length % 8 != 0) return false
     val lengthMod = secret.filter { it != '=' }.length % 8
     val invalidMods = listOf(1, 3, 6)
