67f9a38106b04b370b263347081ec9de8700911e
Mammoth70 <andrey-yakovlev@yandex.ru>
2026-08-03 12:12:32 +0300

    Version 2.10.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
     defaultConfig {
         val versionMajor = 2
         val versionMinor = 10
-        val versionPatch = 11
+        val versionPatch = 12
         applicationId = "ru.mammoth70.totpgenerator"
         minSdk = 28
         targetSdk = 37
